9d2324a9016fad40c85ebb56ba3782e088eec4ea
Naif Mersal <naifmersal@gmail.com>
2024-05-09 18:47:08 +0300

    train video

--- src/models/image.py
+++ src/models/image.py
@@ -83,43 +83,6 @@ class SepConv(nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.conv(x)
 
-class MyModel1(nn.Module): #new
-    def __init__(self, num_classes: int = 1000) -> None:
-
-        super().__init__()
-
-
-            
-        self.Dconvs=nn.Sequential(
-            nn.Conv2d(3,20,3,2,1),
-            nn.BatchNorm2d(20),
-            nn.LeakyReLU(inplace=True),
-            SepConv(20,40,3,stride=2,padding=1,growth_factor=4 ,depth_count=3),# Avgpool-> 56 
-            SepConv(40,40,3,stride=2,padding=1,growth_factor=3 ,depth_count=3),# then Avgpool -> 28 
-            SepConv(40,80,3,stride=2,padding=1,growth_factor=4 ,depth_count=3), # then concate avgpool -> 14
-            SepConv(80,80,3,stride=2,padding=1,growth_factor=3 ,depth_count=3),  # then concate Avgpool -> 7
-            SepConv(80,num_classes,3,padding=1, growth_factor=num_classes//80,depth_count=3),
-            # SepConv(160,320,3, padding=1, growth_factor=3,depth_count=2),
-            ) # then concate avgpool -> 1
-        
-
-
-
-
-        self.fc=nn.Sequential(
-        nn.AdaptiveMaxPool2d(1),
-            nn.Flatten(),
-        )
-        
-        
-
-
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x=self.Dconvs(x)
-        x=self.fc(x)
-        return x
-
 class MyModel2(nn.Module): #new
     def __init__(self, num_classes: int = 1000) -> None:
 
