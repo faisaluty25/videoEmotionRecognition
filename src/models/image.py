73bdaf52184e32c767e85ad902f9f0492ebe4e5a
Naif Mersal <naifmersal@gmail.com>
2024-05-09 13:21:36 +0300

    combine files

--- src/models/image.py
+++ src/models/image.py
@@ -1,6 +1,62 @@
 import torch
 import torch.nn as nn
+import torchvision
+import torchvision.models as t_models
 from torchvision.ops import SqueezeExcitation
+
+
+
+def get_model_transfer_learning(model_name="resnet18", n_classes=50):
+
+    # Get the requested architecture
+    if hasattr(t_models, model_name):
+
+        model_transfer = getattr(t_models, model_name)(pretrained=True)
+
+    else:
+
+        torchvision_major_minor = ".".join(torchvision.__version__.split(".")[:2])
+
+        raise ValueError(f"Model {model_name} is not known. List of available models: "
+                         f"https://pytorch.org/vision/{torchvision_major_minor}/models.html")
+    
+    # detrmine the last layer
+    classifier='fc' if hasattr(model_transfer,'fc') else 'classifier'  
+    
+    # detrmine num_features( this not a good way but it works)
+    try:
+        setattr(model_transfer,classifier, nn.Linear(2,1))
+        x=torch.zeros(1,3,224,224)
+        model_transfer(x)
+    except Exception as e:
+        error=str(e)
+        start_index=error.index('(')
+        end_index=error[start_index:].index('a')
+        in_features=int(error[start_index:start_index+end_index].split('x')[1])
+
+
+
+    # Freeze all parameters in the model
+    # HINT: loop over all parameters. If "param" is one parameter,
+    # "param.requires_grad = False" freezes it
+    # for param in model_transfer.parameters():
+    #     param.requires_grad = False
+    
+
+
+
+    # Add the linear layer at the end with the appropriate number of classes
+    # 1. get numbers of features extracted by the backbone
+    num_ftrs  = in_features
+
+    # 2. Create a new linear layer with the appropriate number of inputs and
+    #    outputs
+    fc=nn.Sequential(nn.Flatten(), nn.Linear(num_ftrs,n_classes))
+               
+        
+    setattr(model_transfer,classifier, fc)
+
+    return model_transfer
 
 class SepConv(nn.Module):
     def __init__(self, in_channels: int , out_channels:int, kernel_size:int, stride=1, padding=0,depth_count:int=2, is_last=False ,growth_factor=1,groups:int=1) -> None:
